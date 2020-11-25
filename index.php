65386824cbf5f08120afc6a87784ce5eb1e6778b
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2020-11-25 17:13:52 -0300

    commit
    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap</title>
+    <title>Uma pagina de teste</title>
 
     <!-- Chamada da biblioteca em CSS interna -->
     <link rel="stylesheet" href="biblioteca_estilos.css">
